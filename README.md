55ed9aa76607725edf91d39c04cf725a2112f89c
mfuentes <fuentes.angel22@hotmail.com>
2020-10-14 15:19:53 -0500

    Testing and readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# pokedex-react
-Pokedex example project in React
+# Pokedex
+
+## Description
+The page allows the user to select a pokemon from a list that is showed on the left of the screen, also allows to search for a specific pokemon, it can be searched by the name or id, it only works by clicking the button to search.
+
+The user can navigate through all the pokemons available by using the arrow buttons in the top of the list.
+
+## Installation
+**Dev Installation**
+These are the steps to install the app and run as developer
+```
+npm install
+npm start
+```
+**Tests**
+The app contains basic testing cases, to execute them use
+```
+npm test
+```
+
+**Prod Installation**
+Steps to install the app in a web server
+```
+npm install
+npm run build
+```
+Copy the build folder into the public folder of web server
--- pokedex/src/App.test.js
+++ pokedex/src/App.test.js
@@ -1,25 +1,51 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 import { act } from 'react-dom/test-utils';
 
-test('contains title', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/pokedex/i);
-  expect(linkElement).toBeInTheDocument();
-});
 
-test('search pokemon', () => {
-  let inputSearch;
-  let button;
-  act(() => {
-    const { getByPlaceholderText, getByText, container } = render(<App />);
-    inputSearch = getByPlaceholderText(/Search/i);
-    button = container.querySelector("button[name = 'bSearch']");
-    fireEvent.change(inputSearch, { target: {value: '132'}});
-    //fireEvent.click(button);
+describe('Basic tests', () =>{
+  test('renders title', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/pokedex/i);
+    expect(linkElement).toBeInTheDocument();
   });
-  //screen.debug();
 
-  expect(inputSearch.value).toBe('132');
-})
+  global.fetch = jest.fn(() => Promise.resolve({
+    json: () => Promise.resolve({
+      count: 1050,
+      next: "https://pokeapi.co/api/v2/pokemon?offset=5&limit=5",
+      previous: null,
+      results: [
+        {
+          name: "bulbasaur",
+          url: "https://pokeapi.co/api/v2/pokemon/1/"
+        },
+        {
+          name: "ivysaur",
+          url: "https://pokeapi.co/api/v2/pokemon/2/"
+        },
+        {
+          name: "venusaur",
+          url: "https://pokeapi.co/api/v2/pokemon/3/"
+        },
+        {
+          name: "charmander",
+          url: "https://pokeapi.co/api/v2/pokemon/4/"
+        },
+        {
+          name: "charmeleon",
+          url: "https://pokeapi.co/api/v2/pokemon/5/"
+        }
+      ]
+    })
+  })
+  )
+
+  test('renders pokemon list', async () => {
+    await act(async () => render(<App />))
+    expect(screen.getByText('BULBASAUR')).toBeInTheDocument();
+    expect(screen.getByText('CHARMANDER')).toBeInTheDocument();
+    expect(screen.getByText(/1050/)).toBeInTheDocument();
+  })
+});
